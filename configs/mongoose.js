ff71519a2c821bebad806a48e875a9f9a0ab555b
Dhairya99999 <110965688+Dhairya99999@users.noreply.github.com>
2024-04-29 20:22:20 +0530

    mongoose.js

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-const url = mongodb+srv://dhairyajan9:Dhairya9@cluster.7a0qckj.mongodb.net/
+const url = "mongodb+srv://dhairyajan9:Dhairya9@cluster.7a0qckj.mongodb.net/"
 
 
 module.exports.connectUsingMongoose = async function(){
